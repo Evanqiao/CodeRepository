3a3eb745293a9e2c1fccbd7cebac0149f86256c2
qiaoyihan <yihqiao@126.com>
2017-03-31 14:57:06 +0800

    add qlog

---
+++ qlog/src/Appender.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: Appender.cpp
+    > Author: qiaoyihan
+    > Email: yihqiao@126 
+    > Created Time: Wed Jun 29 10:54:38 2016
+ ************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <vector>
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
